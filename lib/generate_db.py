213fe62989103e2466d6572bb71a5ab0f76a8dca
Skipper Seabold <jsseabold@gmail.com>
2011-10-18 11:46:52 -0400

    Fist round of optimizations. Some pep8 cleanup

--- lib/generate_db.py
+++ lib/generate_db.py
@@ -2,6 +2,7 @@ import sys
 import math
 import sqlite3
 import numpy as np
+from numpy import argsort
 
 
 ### score functions ###
@@ -10,26 +11,18 @@ import numpy as np
 def get_doc_score(doca, docb, axis=1):
     return .5 * np.sum((doca - docb)**2, axis=axis)
 
-def get_topic_score(topica, topicb):
-    score = 0
-    total = math.pow(abs(math.sqrt(100) - math.sqrt(0)), 2) * len(topica)
-    for term_id in range(len(topica)):
-        thetaa = abs(topica[term_id])
-        thetab = abs(topicb[term_id])
-        score += math.pow(abs(math.sqrt(thetaa) - math.sqrt(thetab)), 2)
-    return 0.5 * score / total
+def get_topic_score(topica, topicb, axis=1):
+    score = np.sum((np.abs(topica)**.5 - np.abs(topicb)**.5)**2, axis=axis)
+    return 0.5 * score / (100. * len(topica))
 
-def get_term_score(terma, termb):
-    score = 0
-    for term_id in range(len(terma)):
-        score += math.pow(terma[term_id] - termb[term_id], 2)
-    return score
-
+def get_term_score(terma, termb, axis=1):
+    return np.sum((terma - termb)**2, axis=axis)
 
 ### write relations to db functions ###
 
 def write_doc_doc(con, cur, gamma_file):
-    cur.execute('CREATE TABLE doc_doc (id INTEGER PRIMARY KEY, doc_a INTEGER, doc_b INTEGER, score FLOAT)')
+    cur.execute('CREATE TABLE doc_doc (id INTEGER PRIMARY KEY, doc_a INTEGER, '
+                'doc_b INTEGER, score FLOAT)')
     cur.execute('CREATE INDEX doc_doc_idx1 ON doc_doc(doc_a)')
     cur.execute('CREATE INDEX doc_doc_idx2 ON doc_doc(doc_b)')
     con.commit()
@@ -47,13 +40,15 @@ def write_doc_doc(con, cur, gamma_file):
         min_doc_idx = np.argsort(distance)[:100]
 
         for b in min_doc_idx:
-            execution_string = 'INSERT INTO doc_doc (id, doc_a, doc_b, score) VALUES(NULL, ?, ?, ?)'
-            cur.execute(execution_string, [str(a]), str(b), distance[b]])
+            execution_string = 'INSERT INTO doc_doc (id, doc_a, doc_b, score) '
+            execution_string += 'VALUES(NULL, ?, ?, ?)'
+            cur.execute(execution_string, [str(a), str(b), distance[b]])
 
     con.commit()
 
 def write_doc_topic(con, cur, gamma_file):
-    cur.execute('CREATE TABLE doc_topic (id INTEGER PRIMARY KEY, doc INTEGER, topic INTEGER, score FLOAT)')
+    cur.execute('CREATE TABLE doc_topic (id INTEGER PRIMARY KEY, doc INTEGER, '
+                'topic INTEGER, score FLOAT)')
     cur.execute('CREATE INDEX doc_topic_idx1 ON doc_topic(doc)')
     cur.execute('CREATE INDEX doc_topic_idx2 ON doc_topic(topic)')
     con.commit()
@@ -79,118 +74,96 @@ def write_topics(con, cur, beta_file, vocab):
     #NOTE: What is the following line for and why doesn't it raise an error?
     topics_file = open(filename, 'a')
 
-    beta = np.loadtxt(beta_file)
-    indices = np.argsort(beta, axis=1)
-    for index in indices: # note this breaks ties differently than old code
+    for topic in open('final.beta', 'r'):
+        topic = map(float, topic.split())
+        index = argsort(topic)
         ins = 'INSERT INTO topics (id, title) VALUES(NULL, ?)'
         buf = "{%s, %s, %s}" % (vocab[index[0]],
                                 vocab[index[1]],
                                 vocab[index[2]])
         cur.execute(ins, [buffer(buf)])
+
     con.commit()
 
 
 def write_topic_term(con, cur, beta_file):
-    cur.execute('CREATE TABLE topic_term (id INTEGER PRIMARY KEY, topic INTEGER, term INTEGER, score FLOAT)')
+    cur.execute('CREATE TABLE topic_term (id INTEGER PRIMARY KEY, topic INTEGER, '
+                'term INTEGER, score FLOAT)')
     cur.execute('CREATE INDEX topic_term_idx1 ON topic_term(topic)')
     cur.execute('CREATE INDEX topic_term_idx2 ON topic_term(term)')
     con.commit()
 
-    topic_no = 0
     topic_term_file = open(filename, 'a')
 
-    beta = np.loadtxt(beta_file)
-    indices = np.argsort(beta, axis=1)
-    for topic_no,topic in enumerate(beta):
+    for topic_no,topic in enumerate(open(beta_file, 'r')):
+        topic = map(float, topic.split())
+        index = argsort(topic)
         for i in range(len(topic)):
             ins = 'INSERT INTO topic_term (id, topic, term, score) '
             ins += 'VALUES(NULL, ?, ?, ?)'
-
-            cur.execute(ins, [topic_no, indices[topic_no,i],
-                              topic[indices[topic_no, i]]])
+            cur.execute(ins, [topic_no, index[i], topic[index[i]]])
 
     con.commit()
 
 
 def write_topic_topic(con, cur, beta_file):
-    cur.execute('CREATE TABLE topic_topic (id INTEGER PRIMARY KEY, topic_a INTEGER, topic_b INTEGER, score FLOAT)')
+    cur.execute('CREATE TABLE topic_topic (id INTEGER PRIMARY KEY, '
+                'topic_a INTEGER, topic_b INTEGER, score FLOAT)')
     cur.execute('CREATE INDEX topic_topic_idx1 ON topic_topic(topic_a)')
     cur.execute('CREATE INDEX topic_topic_idx2 ON topic_topic(topic_b)')
     con.commit()
+
     # for each line in the beta file
     read_file = file(beta_file, 'r')
     topics = []
-    topic_no = 0
     for topic in read_file:
         topics.append(map(float, topic.split()))
-        topic_no = topic_no +1
+    topics = np.asarray(topics)
 
-    topica_count = 0
-    topic_by_topic = []
-    for topica in topics:
-        #topic_sim = r
-        topicb_count = 0
-        for topicb in topics:
-            if topic_by_topic.count((topicb_count, topica_count)) != 0:
-                topicb_count +=1
-                continue
-            score = get_topic_score(topica, topicb)
-            cur.execute('INSERT INTO topic_topic (id, topic_a, topic_b, score) VALUES(NULL, ?, ?, ?)', [topica_count, topicb_count, score])
-
-            topic_by_topic.append((topica_count, topicb_count))
-            topicb_count = topicb_count + 1
-        topica_count = topica_count + 1
-
+    for topica_count,topic in enumerate(topics):
+        scores = get_topic_score(topic, topics)
+        for topicb_count, score in enumerate(scores):
+            ins = 'INSERT INTO topic_topic (id, topic_a, topic_b, score) '
+            ins += 'VALUES(NULL, ?, ?, ?)'
+            con.execute(ins, [topica_count, topicb_count, score])
     con.commit()
 
 
 
 def write_term_term(con, cur, beta_file, no_vocab):
-    v = {}
-
-    cur.execute('CREATE TABLE term_term (id INTEGER PRIMARY KEY, term_a INTEGER, term_b INTEGER, score FLOAT)')
+    cur.execute('CREATE TABLE term_term (id INTEGER PRIMARY KEY, '
+                'term_a INTEGER, term_b INTEGER, score FLOAT)')
     cur.execute('CREATE INDEX term_term_idx1 ON term_term(term_a)')
     cur.execute('CREATE INDEX term_term_idx2 ON term_term(term_b)')
     con.commit()
 
-    for i in range(no_vocab):
-	    v[i] = []
-
+    v = []
     for topic in file(beta_file, 'r'):
-        topic = map(float, topic.split())
-        for i in range(no_vocab):
-            v[i].append(math.sqrt(math.exp(topic[i])))
+        v.append(map(float, topic.split()))
+    v = np.exp(v)**.5
 
-    for terma in range(no_vocab):
-        if terma % 1000 == 0:
-            print "term " + str(terma)
-        term_by_term = {}
-        for termb in range(terma, len(vocab)):
-            score = get_term_score(v[terma], v[termb])
-            if score == 0:
-                continue
-            elif len(term_by_term) < 100:
-                term_by_term[score] = (terma, termb)
-            else:
-                max_score = max(term_by_term.keys())
-                if max_score > score:
-                    del term_by_term[max_score]
-                    term_by_term[score] = (terma, termb)
+    for a in range(len(v)):
+        terma = v[a]
+        score = get_term_score(terma, vv)
+        # drop zeros
+        score[score == 0] = np.inf
+        min_score_idx = np.argsort(score)[:100]
 
-        for term in term_by_term:
-            execution_string = 'INSERT INTO term_term (id, term_a, term_b, score) VALUES(NULL, ?, ?, ?)'
-            cur.execute(execution_string, [term_by_term[term][0], term_by_term[term][1], term])
+        for b in min_score_idx:
+            ins = 'INSERT INTO term_term (id, term_a, term_b, score) '
+            ins += 'VALUES(NULL, ?, ?, ?)'
+            cur.execute(ins, [str(a), str(b), score[b]])
 
     con.commit()
 
 def write_doc_term(con, cur, wordcount_file, no_words):
-    cur.execute('CREATE TABLE doc_term (id INTEGER PRIMARY KEY, doc INTEGER, term INTEGER, score FLOAT)')
+    cur.execute('CREATE TABLE doc_term (id INTEGER PRIMARY KEY, doc INTEGER, '
+                'term INTEGER, score FLOAT)')
     cur.execute('CREATE INDEX doc_term_idx1 ON doc_term(doc)')
     cur.execute('CREATE INDEX doc_term_idx2 ON doc_term(term)')
     con.commit()
 
-    doc_no = 0
-    for doc in file(wordcount_file, 'r'):
+    for doc_no, doc in enumerate(file(wordcount_file, 'r')):
         doc = doc.split()[1:]
         terms = {}
         for term in doc:
@@ -200,10 +173,9 @@ def write_doc_term(con, cur, wordcount_file, no_words):
             score = 0
             if terms.has_key(i):
                 score = terms[i]
-                execution_string = 'INSERT INTO doc_term (id, doc, term, score) VALUES(NULL, ?, ?, ?)'
-                cur.execute(execution_string, [doc_no, i, score])
-
-        doc_no += 1
+                execution_str = 'INSERT INTO doc_term (id, doc, term, score) '
+                execution_str += 'VALUES(NULL, ?, ?, ?)'
+                cur.execute(execution_str, [doc_no, i, score])
 
     con.commit()
 
@@ -212,7 +184,8 @@ def write_terms(con, cur, terms_file):
     con.commit()
 
     for line in open(terms_file, 'r'):
-        cur.execute('INSERT INTO terms (id, title) VALUES(NULL, ?)', [buffer(line.strip())])
+        cur.execute('INSERT INTO terms (id, title) VALUES(NULL, ?)',
+                    [buffer(line.strip())])
 
     con.commit()
 
@@ -221,7 +194,8 @@ def write_docs(con, cur, docs_file):
     con.commit()
 
     for line in open(docs_file, 'r'):
-        cur.execute('INSERT INTO docs (id, title) VALUES(NULL, ?)', [buffer(line.strip())])
+        cur.execute('INSERT INTO docs (id, title) VALUES(NULL, ?)',
+                     [buffer(line.strip())])
 
     con.commit()
 
